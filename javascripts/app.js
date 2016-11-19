05d7fa00268cb1705ca9aadac189de62fd30ea4a
Abby Moore <abbyleighanne@gmail.com>
2016-11-18 18:39:52 -0600

    pull data out of json

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //Promises to read, first, from the categories.json to load that array of objects, then load types.json, then products.json.
 
 
@@ -32,7 +34,7 @@ function getProducts(){
 function getTypes(){
 	return new Promise((resolve, reject) => {
 		$.ajax({
-			url: "data/products.json"
+			url: "data/types.json"
 		}).done(function(data) {
 			resolve(data);
 		}).fail((error) => {
@@ -46,42 +48,17 @@ function checkData(data){
 	console.log("checkData", data); 
 }
 
-function readProducts(productsData){	
-	//console.log(productsData.fairy_sparklers.name);
-	//^output's the name from charred_stump
-	
-	//grab the name of each product
-	var productKeys = Object.keys(productsData);
-	console.log("Product's Keys: ", productKeys);
-
-	//How many products are there? And what's their key?
-	for (var i = 0; i < productKeys.length; i++){
-
-		var getInfo = productKeys[i];
-
-		//use bracket notation to filter down into the data
-		var productName = productsData[getInfo]["name"];
-		var productDescription = productsData[getInfo]["description"];
-
-		//output the data. yay.
-		console.log(productName + " " + productDescription);
-	}
-}
-
-
 getCategories()
 	.then((categoryData) =>{
-		//console.log(categoryData.categories);
-		//readCategory(categoryData.categories);
+		readCategories(categoryData.categories);
 		return getProducts(); 
 	})
 	.then((productsData) => {
 		readProducts(productsData.products[0]);
-		//console.log("productsData", productsData);
 		return getTypes();
-	// })
-	// .then((typesData) => {
-	// 	parseTypes(typesData);
+	})
+	.then((typesData) => {
+		readTypes(typesData.types);
 	});
 
 
